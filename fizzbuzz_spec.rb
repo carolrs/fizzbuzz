05264ed5b94f61f2fbab899f4079a2d2e48a0998
aryadath <67595180+aryadath@users.noreply.github.com>
2023-03-07 10:49:17 +0000

    Created rspec to test code

---
+++ fizzbuzz_spec.rb
@@ -0,0 +1,16 @@
+require './Fizzbuzz'
+
+describe 'Fizzbuzz' do
+  it 'fizzbuzz 3' do
+    expect(fizzbuzz(3)).to eq 'Fizz'
+  end
+  it 'fizzbuzz 5' do
+    expect(fizzbuzz(5)).to eq 'Buzz'
+  end
+  it 'fizzbuzz 15' do
+    expect(fizzbuzz(15)).to eq 'Fizzbuzz'
+  end
+  it 'fizzbuzz 14' do
+    expect(fizzbuzz(14)). to eq 14
+  end
+end
